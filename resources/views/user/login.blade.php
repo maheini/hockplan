d5b349b4ee8ea67f9ea79bc60506c14b176a2771
maheini35 <maheini35@me.com>
2022-10-01 23:59:48 +0200

    finished login blade

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -4,9 +4,16 @@
 
 <div id="login">
     <h2>Anmelden</h2>
-    <form method="POST">
+    <form method="POST" action="/login">
+        @csrf
         <input type="email" name="email" placeholder="Email">
+        @error('email')
+        <p>{{$message}}</p>
+        @enderror
         <input type="password" name="password" placeholder="Passwort">
+        @error('password')
+        <p>{{$message}}</p>
+        @enderror
         <input type="submit" value="Anmelden">
     </form>
 </div>
