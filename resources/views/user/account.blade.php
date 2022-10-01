cfc1fec5cfcec6808f8934eb08a70116a3ffb316
maheini35 <maheini35@me.com>
2022-10-01 23:59:39 +0200

    My Account page

---
+++ resources/views/user/account.blade.php
@@ -0,0 +1,15 @@
+@extends('scaffold/scaffold')
+
+@section('content')
+
+@auth
+    
+<h1>Dein Konto</h1>
+<h3>Benutzername:</h3>
+<p>{{auth()->user()->name}}</p>
+<h3>Email:</h3>
+<p>{{auth()->user()->email}}</p>
+
+@endauth
+
+@endsection
