f9092ffe15460b598af41e2527a0daf741660435
maheini35 <maheini35@me.com>
2022-10-01 23:59:56 +0200

    finished register blade

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -4,11 +4,21 @@
 
 <div id="login">
     <h2>Registrieren</h2>
-    <form method="POST">
-        <input type="text" name="name" placeholder="Benutzername">
-        <input type="email" name="email" placeholder="Email">
-        <input type="password" name="password" placeholder="Passwort">
-        <input type="password" name="password_confirm" placeholder="Passwort bestätigen">
+    <form method="POST" action="/register">
+        @csrf
+        <input type="text" name="name" placeholder="Benutzername" value="{{old('name')}}"/>
+        @error('name')
+        <p>{{$message}}</p>
+        @enderror
+        <input type="email" name="email" placeholder="Email"  value="{{old('email')}}"/>
+        @error('email')
+        <p>{{$message}}</p>
+        @enderror
+        <input type="password" name="password" placeholder="Passwort" value="{{old('password')}}"/>
+        @error('password')
+        <p>{{$message}}</p>
+        @enderror
+        <input type="password" name="password_confirmation" placeholder="Passwort bestätigen"  value="{{old('password_confirmation')}}"/>
         <input type="submit" value="Registrieren">
     </form>
 </div>
